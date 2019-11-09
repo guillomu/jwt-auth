d8c2356c77105b9ca86e3b79f385dbb292fd56bb
GuiGui <groult_guigui@hotmail.com>
2019-11-09 23:24:04 +0100

    [Configuration] Adding CORS Configuration

--- jwt-auth-server/src/main/java/fr/formation/jwtauthserver/SecurityConfig.java
+++ jwt-auth-server/src/main/java/fr/formation/jwtauthserver/SecurityConfig.java
@@ -3,12 +3,19 @@ package fr.formation.jwtauthserver;
 import org.springframework.context.annotation.Bean;
 import org.springframework.http.HttpMethod;
 import org.springframework.security.authentication.AuthenticationManager;
+import org.springframework.security.config.annotation.method.configuration.EnableGlobalMethodSecurity;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.builders.WebSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
 import org.springframework.security.config.http.SessionCreationPolicy;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.security.crypto.password.PasswordEncoder;
+import org.springframework.web.cors.CorsConfiguration;
+import org.springframework.web.cors.CorsConfigurationSource;
+import org.springframework.web.cors.UrlBasedCorsConfigurationSource;
+
+import java.util.Arrays;
 
 @EnableWebSecurity
 public class SecurityConfig extends WebSecurityConfigurerAdapter {
@@ -32,6 +39,11 @@ public class SecurityConfig extends WebSecurityConfigurerAdapter {
 		.antMatchers(HttpMethod.POST, "/api/users").anonymous()
 		// Deny all other accesses
 		.anyRequest().denyAll();
+    }
+
+    @Override
+    public void configure(WebSecurity web){
+        web.ignoring().antMatchers(HttpMethod.OPTIONS, "/oauth/token");
     }
 
     /**
